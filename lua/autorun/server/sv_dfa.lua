6f180076b832eef793df0c7b39bee87821d2eb7c
Redox <mail@redox.to>
2022-10-12 15:21:52 +0200

    Fix damage calculations

--- lua/autorun/server/sv_dfa.lua
+++ lua/autorun/server/sv_dfa.lua
@@ -3,27 +3,25 @@ local lastCheckedChair = 1
 
 local IsValid = IsValid
 
-local punishSpeed = CreateConVar( "dfa_punishspeed", 1000, { FCVAR_ARCHIVE }, "The speed at which the driver should receive punishment.", 0 ):GetInt()
+local punishSpeed = CreateConVar( "dfa_punishspeed", 2500, { FCVAR_ARCHIVE }, "The speed at which the driver should receive punishment.", 0 ):GetInt()
 cvars.AddChangeCallback( "dfa_punishspeed", function( _, _, val )
     punishSpeed = tonumber( val )
 end )
 
-local damageMultiplier = CreateConVar( "dfa_punishspeed", 1, { FCVAR_ARCHIVE }, "The damage multiplier.", 0 ):GetInt()
-cvars.AddChangeCallback( "dfa_punishspeed", function( _, _, val )
-    damageMultiplier = tonumber( val )
-end )
+-- local damageMultiplier = CreateConVar( "dfa_damagemultiplier", 1, { FCVAR_ARCHIVE }, "The damage multiplier.", 0 ):GetInt()
+-- cvars.AddChangeCallback( "dfa_damagemultiplier", function( _, _, val )
+--     damageMultiplier = tonumber( val )
+-- end )
 
 local maxDamage = CreateConVar( "dfa_maxdamage", 10, { FCVAR_ARCHIVE }, "The max damage damage ticks can do.", 0 ):GetInt()
 cvars.AddChangeCallback( "dfa_maxdamage", function( _, _, val )
     maxDamage = tonumber( val )
 end )
 
-
 local function punishSeat( ply, veh, speed )
-    print( ply, veh, speed )
     ply:PrintMessage( HUD_PRINTCENTER, "You are going too fast!" )
-    local damage = math.min( math.floor( ( ( speed / 1000 ) ^ 4 / 10000 ) * damageMultiplier ), maxDamage )
-    ply:ChatPrint( speed .. " " .. damage )
+    local damage = math.floor( ( speed - punishSpeed ) / 1000  * damageMultiplier )
+    ply:ChatPrint( speed - punishSpeed .. " " .. damage )
     veh:TakeDamage( damage, game.GetWorld(), veh )
 end
 
