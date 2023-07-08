927da3fa035862d4158f707f3db8eae530006041
Redox <mail@redox.to>
2023-07-08 15:33:46 +0200

    Filter affected ents more speficially

--- lua/autorun/server/sv_acf_balance_detours.lua
+++ lua/autorun/server/sv_acf_balance_detours.lua
@@ -1,10 +1,19 @@
+local istable = istable
 local entMeta = FindMetaTable( "Entity" )
 
 entMeta.o_SetNotSolid = entMeta.o_SetNotSolid or entMeta.SetNotSolid
 function entMeta:SetNotSolid( solid )
-    if solid and self:IsVehicle() then
-        return
+    if not self:IsVehicle() or solid then
+        return self:o_SetNotSolid( solid )
     end
 
-    self:o_SetNotSolid( solid )
+    -- Only affect wire pod linked seats
+    local entTable = self:GetTable()
+    if not istable( entTable.OnDieFunctions ) then
+        return self:o_SetNotSolid( solid )
+    end
+
+    if not entTable.OnDieFunctions.wire_pod_remove then
+        return self:o_SetNotSolid( solid )
+    end
 end
