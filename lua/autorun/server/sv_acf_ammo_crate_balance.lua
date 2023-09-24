68ba3ca140b4312d99b6d9d33ab0ebd09347f414
legokidlogan <26103433+legokidlogan@users.noreply.github.com>
2023-09-24 17:40:04 -0600

    Don't let ammo crates nuke unmoving bases

---
+++ lua/autorun/server/sv_acf_ammo_crate_balance.lua
@@ -0,0 +1,14 @@
+local IsValid = IsValid
+
+-- Prevent ACF ammo crates from damaging static, unmoving bases.
+hook.Add( "ACF_PreDamageEntity", "ACFBalance_AmmoCrate_DontNukeBases", function( ent, _, dmgInfo )
+    local attacker = dmgInfo:GetAttacker()
+    if attacker:GetClass() ~= "acf_ammo" then return end
+
+    if IsValid( ent:GetParent() ) then return end -- Allow parented ents to be damaged by crates.
+
+    local physObj = ent:GetPhysicsObject()
+    if physObj and physObj:IsMotionEnabled() then return end -- Allow unfrozen ents to be damaged by crates.
+
+    return false
+end )
