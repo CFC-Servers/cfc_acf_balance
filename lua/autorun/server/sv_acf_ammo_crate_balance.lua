e58325b93b863519420b913cb7f538bc7a0df4bf
legokidlogan <26103433+legokidlogan@users.noreply.github.com>
2023-09-24 17:57:08 -0600

    Sorten checks

--- lua/autorun/server/sv_acf_ammo_crate_balance.lua
+++ lua/autorun/server/sv_acf_ammo_crate_balance.lua
@@ -4,10 +4,10 @@ hook.Add( "ACF_PreDamageEntity", "ACFBalance_AmmoCrate_DontNukeBases", function(
     if attacker:GetClass() ~= "acf_ammo" then return end
 
     local parent = ent:GetParent()
-    if parent and parent:IsValid() then return end -- Allow parented ents to be damaged by crates.
+    if parent:IsValid() then return end -- Allow parented ents to be damaged by crates.
 
     local physObj = ent:GetPhysicsObject()
-    if physObj and physObj:IsMotionEnabled() then return end -- Allow unfrozen ents to be damaged by crates.
+    if physObj:IsValid() and physObj:IsMotionEnabled() then return end -- Allow unfrozen ents to be damaged by crates.
 
     return false
 end )
