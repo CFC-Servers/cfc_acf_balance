1ded3b992803a235429d0be1749e75e8cf920b61
legokidlogan <26103433+legokidlogan@users.noreply.github.com>
2023-09-24 17:45:04 -0600

    Use :IsValid()

--- lua/autorun/server/sv_acf_ammo_crate_balance.lua
+++ lua/autorun/server/sv_acf_ammo_crate_balance.lua
@@ -1,11 +1,10 @@
-local IsValid = IsValid
-
 -- Prevent ACF ammo crates from damaging static, unmoving bases.
 hook.Add( "ACF_PreDamageEntity", "ACFBalance_AmmoCrate_DontNukeBases", function( ent, _, dmgInfo )
     local attacker = dmgInfo:GetAttacker()
     if attacker:GetClass() ~= "acf_ammo" then return end
 
-    if IsValid( ent:GetParent() ) then return end -- Allow parented ents to be damaged by crates.
+    local parent = ent:GetParent()
+    if parent and parent:IsValid() then return end -- Allow parented ents to be damaged by crates.
 
     local physObj = ent:GetPhysicsObject()
     if physObj and physObj:IsMotionEnabled() then return end -- Allow unfrozen ents to be damaged by crates.
