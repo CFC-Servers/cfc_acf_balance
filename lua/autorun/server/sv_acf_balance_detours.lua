7b650d4114acac3180d115c3847dfe4269a2efc8
Redox <mail@redox.to>
2023-10-26 22:08:12 +0200

    Add simfphys fix

--- lua/autorun/server/sv_acf_balance_detours.lua
+++ lua/autorun/server/sv_acf_balance_detours.lua
@@ -2,6 +2,16 @@ local entMeta = FindMetaTable( "Entity" )
 
 entMeta.o_SetNotSolid = entMeta.o_SetNotSolid or entMeta.SetNotSolid
 function entMeta:SetNotSolid( solid )
+    if self:GetClass() == "gmod_sent_vehicle_fphysics_base" then
+        self:o_SetNotSolid( solid )
+        return
+    end
+
+    if self:GetClass() == "prop_vehicle_prisoner_pod" and self:GetOwner():GetClass() == "gmod_sent_vehicle_fphysics_base" then
+        self:o_SetNotSolid( solid )
+        return
+    end
+
     if self:IsVehicle() and not solid then return end
     return self:o_SetNotSolid( solid )
 end
