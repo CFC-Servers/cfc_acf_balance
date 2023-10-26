325a43f9bdf83bba43a6936673ff4d52a35ffafa
Redox <mail@redox.to>
2023-10-26 21:58:49 +0200

    Fix vehicle propnotsolid

--- lua/autorun/server/sv_acf_balance_detours.lua
+++ lua/autorun/server/sv_acf_balance_detours.lua
@@ -1,19 +1,20 @@
-local istable = istable
 local entMeta = FindMetaTable( "Entity" )
 
 entMeta.o_SetNotSolid = entMeta.o_SetNotSolid or entMeta.SetNotSolid
 function entMeta:SetNotSolid( solid )
-    if not self:IsVehicle() or solid then
-        return self:o_SetNotSolid( solid )
-    end
+    if self:IsVehicle() and not solid then return end
+    return self:o_SetNotSolid( solid )
+end
 
-    -- Only affect wire pod linked seats
-    local entTable = self:GetTable()
-    if not istable( entTable.OnDieFunctions ) then
-        return self:o_SetNotSolid( solid )
-    end
+entMeta.o_SetSolid = entMeta.o_SetSolid or entMeta.SetSolid
+function entMeta:SetSolid( solid )
+    if self:IsVehicle() and solid == SOLID_NONE then return end
+    return self:o_SetSolid( solid )
+end
 
-    if not entTable.OnDieFunctions.wire_pod_remove then
-        return self:o_SetNotSolid( solid )
-    end
+entMeta.o_SetSolidFlags = entMeta.o_SetSolidFlags or entMeta.SetSolidFlags
+function entMeta:SetSolidFlags( flags )
+    local solid = bit.band( flags, FSOLID_NOT_SOLID )
+    if self:IsVehicle() and solid ~= 0 then return end
+    return self:o_SetSolidFlags( flags )
 end
