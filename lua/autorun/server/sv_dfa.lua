9bab2a97bb05ff2b083ab825e03ceba70d6f8ce7
StrawWagen <64710817+StrawWagen@users.noreply.github.com>
2023-10-26 16:49:32 -0700

    Rework to use GetPos derived velocity
    Couldn't help myself, reworked most of the code aswell.

--- lua/autorun/server/sv_dfa.lua
+++ lua/autorun/server/sv_dfa.lua
@@ -1,8 +1,7 @@
 local activeVehicles = {}
-local IsValid = IsValid
-local engine_TickCount = engine.TickCount
+local _IsValid = IsValid
 
-local punishAccel = CreateConVar( "dfa_punishaccel", 425, { FCVAR_ARCHIVE }, "The acceleration at which the driver should receive punishment.", nil ):GetInt()
+local punishAccel = CreateConVar( "dfa_punishaccel", 525, { FCVAR_ARCHIVE }, "The acceleration at which the driver should receive punishment.", nil ):GetInt()
 local warningScale = 0.2
 local warningStartOffset = punishAccel * warningScale
 cvars.AddChangeCallback( "dfa_punishaccel", function( _, _, val )
@@ -15,69 +14,112 @@ cvars.AddChangeCallback( "dfa_damagemultiplier", function( _, _, val )
     damageMultiplier = tonumber( val )
 end )
 
-local checkInterval = CreateConVar( "dfa_interval", 5, { FCVAR_ARCHIVE }, "The amount of ticks between acceleration checks.", nil ):GetInt()
-cvars.AddChangeCallback( "dfa_interval", function( _, _, val )
-    checkInterval = tonumber( val )
+local maxDamage = CreateConVar( "dfa_peakdamage", 10, { FCVAR_ARCHIVE }, "Peak damage for one 'check interval', can prevent instant death." ):GetFloat()
+cvars.AddChangeCallback( "dfa_peakdamage", function( _, _, val )
+    maxDamage = tonumber( val )
+end )
+
+local nextCheckTimeOffset = CreateConVar( "dfa_checkinterval", 0.1, { FCVAR_ARCHIVE }, "The amount of ticks between acceleration checks.", nil ):GetFloat()
+cvars.AddChangeCallback( "dfa_checkinterval", function( _, _, val )
+    nextCheckTimeOffset = tonumber( val )
 end )
 
 
 local function damageVehicle( veh, driver, accel )
     local accelDiff = accel - punishAccel
     local accelDiffDivided = accelDiff / 14 --magic number, 14 results in fair feeling damage
-    local damage = math.Clamp( accelDiffDivided, 0, 200 )
-    damage = math.Round( damage, 2 )
+    local damage = math.Round( accelDiffDivided, 2 )
     damage = damage * damageMultiplier
+    damage = math.Clamp( damage, 0, maxDamage )
 
     local world = game.GetWorld()
 
-    if IsValid( driver ) then
+    if _IsValid( driver ) then
         driver:TakeDamage( damage, world, world )
     else
         veh:TakeDamage( damage, world, world )
     end
 end
 
+local math_abs = math.abs
+local _CurTime = CurTime
+
+local function getVelocityBulletproof( ent )
+    local currPos = ent:GetPos()
+    local currTime = _CurTime()
+    local oldPos = ent.DFAOldVelocityPos
+    local oldTime = ent.DFALastVelCheckTime
+    ent.DFAOldVelocityPos = currPos
+    ent.DFALastVelCheckTime = currTime
+
+    debugoverlay.Line( oldPos, currPos, 15, color_white, true )
+
+    local deltaTime = math_abs( currTime - oldTime )
+
+    local vel = currPos - oldPos
+    vel = vel / deltaTime
+
+    return vel
+end
+
+local function startTrackingVehicle( veh )
+    activeVehicles[veh] = true
+    veh.DFANextCheck = 0
+    veh.DFALastVelocity = vector_origin
+    veh.DFAOldVelocityPos = veh:GetPos()
+    veh.DFALastVelCheckTime = _CurTime()
+end
+
+local function stopTrackingVehicle( veh )
+    activeVehicles[veh] = nil
+    if not _IsValid( veh ) then return end
+    veh.DFANextCheck = nil
+    veh.DFALastVelocity = nil
+    veh.DFALastVelCheckTime = nil
+    veh.DFAOldVelocityPos = nil
+end
+
 local blackoutScaleDivisor = 2 -- how quickly should blackout ramp up? 4 for 4x as fast, 2 for 2x as fast
 local clampMagicNumber = 255 / blackoutScaleDivisor
 
-local function checkVehicle( veh, trackEnt )
-    if not IsValid( veh ) or not IsValid( trackEnt ) then
-        activeVehicles[veh] = nil
+local function checkVehicle( veh )
+    if not _IsValid( veh ) then
+        stopTrackingVehicle( veh )
         return
     end
 
     local driver = veh:GetDriver()
-    if not IsValid( driver ) then
-        activeVehicles[veh] = nil
+    if not _IsValid( driver ) then
+        stopTrackingVehicle( veh )
         return
     end
 
-    local tickCount = engine_TickCount()
-    local nextCheck = trackEnt.DFANextCheck
+    local curTime = _CurTime()
+    local nextCheck = veh.DFANextCheck
 
-    if not nextCheck then
-        trackEnt.DFANextCheck = tickCount + checkInterval
-        return
+    if curTime < nextCheck then return end
+
+    veh.DFANextCheck = curTime + nextCheckTimeOffset
+
+    --if CFCPvp and not driver:IsInPvp() then return end
+    if veh.IsSimfphyscar then return end
+
+    local currVelocity = getVelocityBulletproof( veh )
+
+    local lastVelocity = veh.DFALastVelocity
+    veh.DFALastVelocity = currVelocity
+
+    if currVelocity == vector_origin and lastVelocity == vector_origin then
+        veh.DFANextCheck = curTime + nextCheckTimeOffset * 5
+
     end
 
-    if tickCount < nextCheck then
-        return
-    end
-    trackEnt.DFANextCheck = tickCount + checkInterval
+    print( lastVelocity, currVelocity )
 
-    if CFCPvp and not driver:IsInPvp() then return end
-    if trackEnt.IsSimfphyscar then return end
+    local accel = ( lastVelocity - currVelocity ):Length()
+    local oldAccel = veh.oldBlackoutAcceleration or accel
 
-    local lastVelocity = trackEnt.DFALastVelocity
-    trackEnt.DFALastVelocity = trackEnt:GetVelocity()
-    if not lastVelocity then
-        return
-    end
-
-    local accel = ( lastVelocity - trackEnt:GetVelocity() ):Length()
-    local oldAccel = trackEnt.oldBlackoutAcceleration or accel
-
-    trackEnt.oldBlackoutAcceleration = accel
+    veh.oldBlackoutAcceleration = accel
 
     local averageAccel = ( accel + oldAccel ) / 2 -- use average so random, insane 1 tick acceleration isnt as crazy
 
@@ -97,33 +139,20 @@ local function checkVehicle( veh, trackEnt )
 end
 
 local function runCheck()
-    for veh, trackEnt in pairs( activeVehicles ) do
-        checkVehicle( veh, trackEnt )
+    for veh, _ in pairs( activeVehicles ) do
+        checkVehicle( veh )
     end
 end
 
 hook.Add( "Think", "DFA_CheckAcceleration", runCheck )
 
 hook.Add( "PlayerEnteredVehicle", "DFA_RegisterSeat", function( _, veh )
-    local trackEnt = veh
-    for _ = 1, 20 do
-        local parent = trackEnt:GetParent()
-        if not IsValid( parent ) then
-            break
-        else
-            trackEnt = parent
-        end
-    end
-    activeVehicles[veh] = trackEnt
+    startTrackingVehicle( veh )
 end )
 
 hook.Add( "PlayerLeaveVehicle", "DFA_UnregisterSeat", function( driver, veh )
-    local trackEnt = activeVehicles[veh]
+    local isTracking = activeVehicles[veh]
     driver:SetNWInt( "DFA_BlackingOut", 0 )
-    if not IsValid( trackEnt ) then return end
-    trackEnt.DFALastCheck = nil
-    trackEnt.DFALastVelocity = nil
-    veh.DFALastCheck = nil
-    veh.DFALastVelocity = nil
-    activeVehicles[veh] = nil
+    if not isTracking then return end
+    stopTrackingVehicle( veh )
 end )
